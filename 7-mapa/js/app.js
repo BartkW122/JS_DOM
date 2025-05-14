d2941128a521912447128a80ef73dd9dce412023
Bartosz Wiaderny <10376794u@qed.local>
2025-05-14 15:22:18 +0200

    poprawiki

--- 7-mapa/js/app.js
+++ 7-mapa/js/app.js
@@ -16,7 +16,7 @@ let map_marker=document.querySelectorAll(".map-marker");
 let map_tooltip=document.querySelector(".map-tooltip");
 
 map_marker.forEach(item=>{
-   //console.log(item)
+   
    let result=window.getComputedStyle(item)
    let left_p=parseInt(result.getPropertyValue('left'))
    let top_p=parseInt(result.getPropertyValue('top'))
@@ -28,37 +28,39 @@ map_marker.forEach(item=>{
       
       h2.innerHTML=item.getAttribute('data-name');
       
-      div.append("Population:");
+      div.innerHTML="Population:";
       strong.innerHTML=item.getAttribute('data-population')
-      div.appendChild(strong)
       
+      div.appendChild(strong)
+
       map_tooltip.appendChild(h2)
       map_tooltip.appendChild(div)
-      let left=parseInt(result.getPropertyValue('left'))+30;
+
+      let left=left_p+30;
       item.style.left=left+"px";
-      let top=parseInt(result.getPropertyValue('top'))+30;
+      let top=top_p+30;
       item.style.top=top+"px";
 
+     
       console.log(map_tooltip)
-      
-      
-      let result2=window.getComputedStyle(map_tooltip)
-      let left2=parseInt(result.getPropertyValue('left'));
+      item.addEventListener('mousemove',()=>{
+         let result2=window.getComputedStyle(map_tooltip)
+         let left2=parseInt(result.getPropertyValue('left'))+30;
          map_tooltip.style.left=left2+"px";
-      console.log(left2)
-      let top2=parseInt(result.getPropertyValue('top'))+30;
+      
+         let top2=parseInt(result.getPropertyValue('top'))+30;
          map_tooltip.style.top=top2+"px";
-      
-         
-      
+      })
    })
    item.addEventListener('mouseout',()=>{
       item.style.left=left_p+"px";
       item.style.top=top_p+"px";
 
-      map_tooltip.removeChild(h2)
+      div.removeChild(strong)
       map_tooltip.removeChild(div)
-      //map_tooltip.remove("Population:");
+      map_tooltip.removeChild(h2)
+     
+
       map_tooltip.style.left="-9999px";
       map_tooltip.style.top="-9999px";
    })
